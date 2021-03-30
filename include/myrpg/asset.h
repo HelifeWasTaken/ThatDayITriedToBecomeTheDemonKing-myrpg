05aeb9422be78fa7d00a857ef688dd774f48a7c0
thibault <thibault.dufaure-de-citres@epitech.eu>
2021-03-30 09:51:15 +0400

    change name for ath

--- include/myrpg/asset.h
+++ include/myrpg/asset.h
@@ -24,7 +24,7 @@
     #define MENU_BUTTON_RECT IRECT(0, 0, MENU_BUTTON_SPT_W, MENU_BUTTON_SPT_H)
     #define BUTTON_MENU_POS (int[]) {WINDOW_H / 3,\
         WINDOW_H / 2,  WINDOW_H / 1.5}
-    
+
     #define QUIT_BUTTON_SPT "asset/menu/quit.png"
     #define PLAY_BUTTON_SPT "asset/menu/play.png"
     #define SETTING_BUTTON_SPT "asset/menu/setting.png"
@@ -46,7 +46,7 @@
     #define ATH_ICON (char *[]) {CHAR_ICON, EQUIP_ICON, INVENTORY_ICON,\
         QUEST_ICON, SKILL_ICON, OPTION_ICON}
 
-    #define PLAYER_ATH "asset/ath/player_ath_test.png"
+    #define PLAYER_ATH "asset/ath/player_ath.png"
     #define PLAYER_ATH_W 410
     #define PLAYER_ATH_H 150
     #define PLAYER_ATH_POS_Y 860
