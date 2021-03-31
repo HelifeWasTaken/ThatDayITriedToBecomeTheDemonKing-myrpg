aa09f0add7c0386dd0e392f696c205ec5dcbbf8d
thibault <thibault.dufaure-de-citres@epitech.eu>
2021-03-31 14:54:58 +0400

    add new map

--- src/entities/map_loader/load_vertex_array.c
+++ src/entities/map_loader/load_vertex_array.c
@@ -7,6 +7,14 @@
 
 #include "myrpg/map.h"
 
+static void load_empty_alpha(sfVertex *quad)
+{
+    quad[0].color = sfColor_fromRGBA(0, 0, 0, 0);
+    quad[1].color = sfColor_fromRGBA(0, 0, 0, 0);
+    quad[2].color = sfColor_fromRGBA(0, 0, 0, 0);
+    quad[3].color = sfColor_fromRGBA(0, 0, 0, 0);
+}
+
 static void load_vertex_quad(map_rect_t *size, sfTexture *tileset,
     struct vertex_loader_data *pos, vertice_data_loader_t *vertice)
 {
@@ -18,6 +26,10 @@ static void load_vertex_quad(map_rect_t *size, sfTexture *tileset,
         (pos->i + pos->j * size->mapx) * 4);
 
     id_rot.id -= vertice->firstgid;
+    if (id_rot.id == -1) {
+        load_empty_alpha(quad);
+        return;
+    }
     tu = id_rot.id % (i64_t)(sfTexture_getSize(tileset).x / size->tilex);
     tv = id_rot.id / (i64_t)(sfTexture_getSize(tileset).x / size->tilex);
     quad[0].position = VEC2F(pos->i * size->tilex, pos->j * size->tiley);
@@ -25,8 +37,6 @@ static void load_vertex_quad(map_rect_t *size, sfTexture *tileset,
     quad[2].position = VEC2F((pos->i + 1) *
             size->tilex, (pos->j + 1) * size->tiley);
     quad[3].position = VEC2F(pos->i * size->tilex, (pos->j + 1) * size->tiley);
-    if (id_rot.id == -1)
-        return;
     set_texture_coords(&(vtx_load_roat_data_t){tu, tv, quad, size, id_rot.rot});
 }
 
