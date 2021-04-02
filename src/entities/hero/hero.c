0ff3e659ac188bf0dffe8407af2ff4799d5880cc
Mattis DALLEAU <mattis.dalleau@epitech.eu>
2021-04-02 21:44:33 +0400

    Using new define hero scale

--- src/entities/hero/hero.c
+++ src/entities/hero/hero.c
@@ -42,7 +42,7 @@ bool create_hero(game_t *game UNUSED, entity_t *entity)
     hero->entity->pos = MAP_FILES[game->scene->world_id].p_info.pos;
     entity->instance = hero;
     hero->entity->z = MAP_FILES[game->scene->world_id].p_info.z_player;
-    sfSprite_setScale(hero->sprite, VEC2F(0.5, 0.5
+    sfSprite_setScale(hero->sprite, HERO_SCALE);
     return (true);
 }
 
