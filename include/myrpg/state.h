c96b310f7f187d1d1543c094e45326d0abcb5641
Alexis Cheron <alexisnorech@gmail.com>
2021-04-18 00:15:22 +0400

    added game_save_t

--- include/myrpg/state.h
+++ include/myrpg/state.h
@@ -11,6 +11,7 @@
 #include "distract/entity.h"
 #include "myrpg/game.h"
 #include "define.h"
+#include <stdint.h>
 
     typedef struct parameters {
         float music_vol;
@@ -21,8 +22,21 @@
         bool voice_muted;
     } parameters_t;
 
+    typedef struct game_save {
+        ///
+        /// Map to load when loading save
+        ///
+        int map_id;
+
+        ///
+        /// Position where to spawn player in map
+        ///
+        sfVector2f player_pos;
+    } game_save_t;
+
     typedef struct game_state {
         parameters_t params;
+        game_save_t save;
     } game_state_t;
 
 #endif /* !PARAMETERS_H_ */
