95a54cd4b2311679c22fe079201ac64438ab51d0
Alexis Cheron <alexisnorech@gmail.com>
2021-04-20 23:57:33 +0400

    fixed play_sound and pausing music on scene change

--- lib/distract/src/scene/await_scene.c
+++ lib/distract/src/scene/await_scene.c
@@ -5,14 +5,46 @@
 ** Source code
 */
 
+#include "distract/resources.h"
 #include "distract/scene.h"
+#include <SFML/Audio/Music.h>
 #include <stdio.h>
+
+static void pause_music(scene_t *parent_scene)
+{
+    resource_t **resources = (resource_t **)parent_scene->resources->values;
+
+    for (size_t i = 0; i < parent_scene->resources->capacity; i++) {
+        if (resources[i] == NULL)
+            continue;
+        if(resources[i]->type == R_MUSIC) {
+            sfMusic_pause(resources[i]->music);
+        }
+        if(resources[i]->type == R_SOUND) {
+            sfSound_pause(resources[i]->sound);
+        }
+    }
+}
+
+static void resume_music(scene_t *parent_scene)
+{
+    resource_t **resources = (resource_t **)parent_scene->resources->values;
+
+    for (size_t i = 0; i < parent_scene->resources->capacity; i++) {
+        if (resources[i] == NULL)
+            continue;
+        if(resources[i]->type == R_MUSIC) {
+            sfMusic_play(resources[i]->music);
+        }
+    }
+}
 
 int await_scene(game_t *game, int scene_id)
 {
     int code;
     scene_t *parent_scene = game->scene;
 
+    pause_music(parent_scene);
     game->scene = allocate_scene();
     if (game->scene == NULL)
         return (-1);
@@ -20,5 +52,6 @@ int await_scene(game_t *game, int scene_id)
     code = load_pending_scene(game);
     deallocate_scene(game->scene);
     game->scene = parent_scene;
+    resume_music(parent_scene);
     return (code);
 }
