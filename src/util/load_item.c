91c72705d7b197a86c8bc41f22742053f408c779
Mattis DALLEAU <mattis.dalleau@epitech.eu>
2021-05-04 21:30:37 +0400

    Update load_item.c

--- src/util/load_item.c
+++ src/util/load_item.c
@@ -87,7 +87,6 @@ void destroy_item(item_t *item)
     free(item);
 }
 
-
 bool init_item(struct json *conf, item_t *item)
 {
     DESER_LOOP(conf, item, ITEM_JSON);
@@ -103,7 +102,6 @@ bool load_n_item(game_t *game, usize_t n, json_t *json)
         return (false);
     }
     for (usize_t index = 0; index != n; index++) {
-        printf("%ld", index);
         item = (item_t){0};
         if (init_item(&item_arr->data[index],
             &item) == false) {
